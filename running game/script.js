4c65bb799d37f429cc237e5ad228cd4933768abe
Alfred <toasthero97@gmail.com>
2025-10-29 18:50:50 +0100

    Man in not relative anymore

--- running game/script.js
+++ running game/script.js
@@ -7,8 +7,9 @@ import * as Util from "./util.js";
 
 
 let speed = 0.007;
-let jump = 0.005
+let jump = 0.005;
 let lastKeyPressed = ' ';
+let score = 0;
 
 //Objects
 
@@ -23,7 +24,7 @@ const manOneObj = {
   l: 80,
   a: 1,
 }
-const thingTwoObj = {
+const trapziodOneObj = {
   x: 0.6,
   y: 0.7,
   width: 90,
@@ -33,6 +34,31 @@ const thingTwoObj = {
   s: 0,
   l: 50,
   a: 0,
+  rot: 0,
+}
+const trapziodLeftObj = {
+  x: 0,
+  y: 420,
+  width: 130,
+  height: 80,
+  r: 0,
+  h: 100,
+  s: 0,
+  l: 50,
+  a: 0,
+  rot: 180,
+}
+const trapziodRightObj = {
+  x: 0,
+  y: 420,
+  width: 130,
+  height: 80,
+  r: 0,
+  h: 100,
+  s: 0,
+  l: 50,
+  a: 0,
+  rot: 180,
 }
 const manOneShadowObj = {
   x: 0.4,
@@ -44,19 +70,21 @@ const manOneShadowObj = {
   s: 0,
   l: 60,
   a: 0.61,
+  rot: 0,
 }
 const barOneObj = {
   x: 0.5,
   y: 0.754,
-  width: 460,
+  width: 464,
   height: 5,
   r: 0,
   h: 100,
   s: 0,
   l: 0,
   a: 1,
+  rot: 0,
 }
-const BarTwoObj = {
+const barTwoObj = {
   x: 0.5,
   y: 0.646,
   width: 415,
@@ -66,6 +94,7 @@ const BarTwoObj = {
   s: 0,
   l: 0,
   a: 1,
+  rot: 0,
 }
 const gameOverObj = {
   x: 0.6,
@@ -77,6 +106,7 @@ const gameOverObj = {
   s: 100,
   l: 50,
   a: 0.61,
+  rot: 0,
 }
 
 
@@ -84,48 +114,85 @@ const gameOverObj = {
 
 // Settings variables should contain all of the "fixed" partsr
 function setAppearanceProperties (obj,name) {
-  const {x,y,width,height,r,h,s,l,a} = obj
+  const {x,y,width,height,r,h,s,l,a,rot} = obj
+  Util.setRotation(rot,name)
   Util.setPosition(x, y, name)
   Util.setSize(width, height, name)
   Util.setRoundedness(r,name)
   Util.setColour(h,s,l,a,name)
 }
+function setAppearancePropertiesPixels (obj,name) {
+  const {x,y,width,height,r,h,s,l,a,rot} = obj
+  Util.setRotation(rot,name)
+  Util.setSize(width, height, name)
+  Util.setRoundedness(r,name)
+  Util.setColour(h,s,l,a,name)
+}
+function isOverlapping(elOne, elTwo) {
+  const rect1 = elOne.getBoundingClientRect();
+  const rect2 = elTwo.getBoundingClientRect();
+
+  const overlap = !(
+    rect1.right < rect2.left ||   // elOne is to the left of elTwo
+    rect1.left > rect2.right ||   // elOne is to the right of elTwo
+    rect1.bottom < rect2.top ||   // elOne is above elTwo
+    rect1.top > rect2.bottom      // elOne is below elTwo
+  );
+
+  return overlap; 
+}
 
 
 // Code that runs over and over again
 function loop() {
-  setAppearanceProperties(manOneShadowObj,thingThree);
-  setAppearanceProperties(manOneObj,thingOne);
-  setAppearanceProperties(thingTwoObj,thingTwo);
-  setAppearanceProperties(barOneObj,thingFour);
-  setAppearanceProperties(BarTwoObj,thingFive);
-  setAppearanceProperties(gameOverObj,thingSix);
+  setAppearancePropertiesPixels(manOneShadowObj,manOneShadow);
+   Util.setPositionPixels(barOneObj.x*window.innerWidth-manOneObj.width,manOneObj.y*window.innerHeight,manOne)
+  setAppearancePropertiesPixels(manOneObj,manOne);
+   Util.setPositionPixels(barOneObj.x*window.innerWidth-manOneObj.width,manOneShadowObj.y*window.innerHeight,manOneShadow)
+  setAppearanceProperties(trapziodOneObj,trapziodOne);
+  setAppearanceProperties(barOneObj,barOne);
+  setAppearanceProperties(barTwoObj,barTwo);
+  setAppearanceProperties(gameOverObj,gameOver);
+  setAppearancePropertiesPixels(trapziodLeftObj,trapziodLeft);
+  setAppearancePropertiesPixels(trapziodRightObj,trapziodRight);
+   Util.setPositionPixels(barOneObj.x*window.innerWidth+barOneObj.width/2-trapziodRightObj.width+36, trapziodRightObj.y, trapziodRight)
+   Util.setPositionPixels(barOneObj.x*window.innerWidth-barOneObj.width/2-trapziodLeftObj.width-36, trapziodLeftObj.y, trapziodLeft)
  
-  
-  
+  if (isOverlapping(manOne, gameOver)) {
+  console.log("Boxes are overlapping!");
+} else {
+  console.log("No overlap detected.");
+}
+ 
   window.requestAnimationFrame(loop);
 }
 
 // Setup
 
 function setup() {
-  Util.createThing("thingFive","thingThree");
-  Util.createThing("thingFour","thingThree");
-  Util.createThing("thingTwo","thingTwo");
-  Util.createThing("thingThree","thingOne");
-  Util.createThing("thingOne","thingOne");
-  Util.createThing("thingSix","thingOne")
+  Util.createThing("barTwo","thingThree");
+  Util.createThing("barOne","thingThree");
+  Util.createThing("trapziodOne","thingTwo");
+  Util.createThing("manOneShadow","thingOne");
+  Util.createThing("manOne","thingOne");
+  Util.createThing("gameOver","thingOne");
+  Util.createThing("trapziodLeft","thingFive");
+  Util.createThing("trapziodRight","thingFour");
 
   document.addEventListener("keydown", (event) => {
     if(lastKeyPressed === 'KeyY' && event.code === 'KeyH'){
-      thingTwoObj.x=thingTwoObj.x-speed; // moves obsticles to the left
-      gameOverObj.x=thingTwoObj.x
+      trapziodOneObj.x=trapziodOneObj.x-speed; // moves obsticles to the left
+      gameOverObj.x=trapziodOneObj.x
+      score+=1
       lastKeyPressed = 'KeyH';
+      console.log('score: '+score)
     }
     if(lastKeyPressed === 'KeyH' && event.code === 'KeyY'){
-      thingTwoObj.x=thingTwoObj.x-speed;
-      gameOverObj.x=thingTwoObj.x
+      trapziodOneObj.x=trapziodOneObj.x-speed;
+      gameOverObj.x=trapziodOneObj.x
+      score+=1
       lastKeyPressed = 'KeyY';
+      console.log('score: '+score)
     }
 
   document.addEventListener("keydown", (event)=> {    
@@ -144,6 +211,7 @@ function setup() {
   lastKeyPressed = event.code
   console.log('lastkey '+event.code)
   console.log('lastkey string '+lastKeyPressed)
+  
 });
   
   window.requestAnimationFrame(loop);
